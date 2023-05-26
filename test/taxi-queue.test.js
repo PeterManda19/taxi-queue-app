2c5dc4aa49bc553f07215d9ced3b23aa216faf0c
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-26 11:38:59 +0200

    Working on Queue counters stored in localStorage

--- test/taxi-queue.test.js
+++ test/taxi-queue.test.js
@@ -184,28 +184,30 @@ describe('The taxi queue app', () => {
 
 describe('LocalStorage Tests', () => {
 	beforeEach(() => {
-	  // Clear localStorage before each test
-	  localStorage.clear();
+		// Clear localStorage before each test
+		localStorage.clear();
 	});
   
 	it('Queue counters are stored in localStorage', () => {
-	  // Simulate actions that update the queue counters
-	  taxiQueue.joinQueue();
-	  taxiQueue.joinTaxiQueue();
-  
-	  // Update queue counters in localStorage
-	  updateQueueCountersInLocalStorage();
-  
-	  // Check if the values are correctly stored in localStorage
-	  assert.strictEqual(localStorage.getItem('passengerQueueCount'), '1');
-	  assert.strictEqual(localStorage.getItem('taxiQueueCount'), '1');
+	  	const taxiQueue = TaxiQueue();
+
+		// Simulate actions that update the queue counters
+		taxiQueue.joinQueue();
+		taxiQueue.joinTaxiQueue();
+	
+		// Update queue counters in localStorage
+		updateQueueCountersInLocalStorage();
+	
+		// Check if the values are correctly stored in localStorage
+		assert.strictEqual(localStorage.getItem('passengerQueueCount'), '1');
+		assert.strictEqual(localStorage.getItem('taxiQueueCount'), '1');
 	});
   
 	it('Factory Function constructor initializes queue counters', () => {
-	  const testTaxiQueue = TaxiQueue();
-  
-	  // Check the initial values of the queue counters
-	  assert.strictEqual(testTaxiQueue.queueLength(), 0);
-	  assert.strictEqual(testTaxiQueue.taxiQueueLength(), 0);
+		const testTaxiQueue = TaxiQueue();
+	
+		// Check the initial values of the queue counters
+		assert.strictEqual(testTaxiQueue.queueLength(), 0);
+		assert.strictEqual(testTaxiQueue.taxiQueueLength(), 0);
 	});  
 });
