b8c74710d497838ffad70a0ee1253c28de5b5d30
PeterManda19 <peter.pmanda.manda@gmail.com>
2023-05-29 11:28:00 +0200

    working on localStorage functionality

--- test/taxi-queue.test.js
+++ test/taxi-queue.test.js
@@ -181,33 +181,3 @@ describe('The taxi queue app', () => {
 
 	});
 });
-
-describe('LocalStorage Tests', () => {
-	beforeEach(() => {
-		// Clear localStorage before each test
-		localStorage.clear();
-	});
-  
-	it('Queue counters are stored in localStorage', () => {
-	  	const taxiQueue = TaxiQueue();
-
-		// Simulate actions that update the queue counters
-		taxiQueue.joinQueue();
-		taxiQueue.joinTaxiQueue();
-	
-		// Update queue counters in localStorage
-		updateQueueCountersInLocalStorage();
-	
-		// Check if the values are correctly stored in localStorage
-		assert.strictEqual(localStorage.getItem('passengerQueueCount'), '1');
-		assert.strictEqual(localStorage.getItem('taxiQueueCount'), '1');
-	});
-  
-	it('Factory Function constructor initializes queue counters', () => {
-		const testTaxiQueue = TaxiQueue();
-	
-		// Check the initial values of the queue counters
-		assert.strictEqual(testTaxiQueue.queueLength(), 0);
-		assert.strictEqual(testTaxiQueue.taxiQueueLength(), 0);
-	});  
-});
